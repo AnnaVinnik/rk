2ef20ef7fc982f6061a2f8bf52bcbf947e83f483
AnnaVinnik <vinnik.anna@rambler.ru>
2018-04-22 22:41:49 +0700

    read key

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int rk_readkey(int *c){
+	read(2, c, 2);
+return *c;
+}
+
+int main()
+{
+int c;
+//rk_readkey(c);
+read(2, &c, 2);
+printf("c = %d\n", c);
+printf("%c\n", c);
+	return 0;
+}
